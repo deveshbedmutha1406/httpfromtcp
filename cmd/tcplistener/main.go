ec153190d344d816a07a7a2f70d8a5ad009829bf
deveshbedmutha1406 <deveshbedmutha500@gmail.com>
2026-02-09 13:16:14 +0530

    tcp connected to http and parsing of header begins

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -2,43 +2,10 @@ package main
 import (
 	"log"
 	"fmt"
-	"bytes"
-	"io"
 	"net"
+	"boot.theprimeagen.tv/internal/request"
 )
 
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	out := make(chan string, 1)
-
-	go func(){
-		defer f.Close()	// before you leave do this
-		defer close(out)	// closing channel
-
-		str := ""
-
-		for {
-			data := make([]byte, 8)
-			n, err := f.Read(data)
-			if err != nil {
-				break
-			}
-			data = data[:n]	// read only n bytes which read.
-			// when new line char then put it in chan.
-			if i := bytes.IndexByte(data, '\n'); i != -1 {
-				str += string(data[:i])
-				data = data[i + 1:]
-				out <- str
-				str = ""
-			}
-			str += string(data)
-		}
-		if len(str) != 0 {
-			out <- str
-		}
-	}()
-
-	return out
-}
 
 
 
@@ -54,10 +21,15 @@ func main(){
 		if err != nil {
 			log.Fatal("error", "error", err)
 		}
-
-		for line := range getLinesChannel(conn) {
-			fmt.Printf("read: %s\n", line) 
+		r, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Fatal("error", "error", err)
 		}
+		
+		fmt.Printf("Request line:\n")
+		fmt.Printf("- Method: %s:\n", r.RequestLine.Method)
+		fmt.Printf("- Target: %s\n", r.RequestLine.RequestTarget)
+		fmt.Printf("- Version: %s\n", r.RequestLine.HttpVersion)
 	}
 
 }
