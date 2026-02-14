6783501f7df59f8eaeb8b88f825f4c8e49d50f4d
deveshbedmutha1406 <deveshbedmutha500@gmail.com>
2026-02-14 23:01:21 +0530

    Adding Response Module

---
+++ internal/server/server.go
@@ -0,0 +1,52 @@
+package server
+
+import (
+	"net"
+	"fmt"
+	"io"
+	"boot.theprimeagen.tv/internal/response"
+)
+
+type Server struct {
+	closed bool
+}
+
+func runConnection(_s *Server, conn io.ReadWriteCloser){
+	defer conn.Close()
+	headers := response.GetDefaultHeaders(0)
+	response.WriteStatusLine(conn, response.StatusOk)
+	response.WriteHeaders(conn, headers)
+}
+
+func runServer(s *Server, listener net.Listener) error {
+
+	for {
+		conn, err := listener.Accept()
+		if s.closed {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		go runConnection(s, conn)
+	}	 
+	
+}
+
+func Serve(port uint16) (*Server, error) {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return nil, err
+	}
+
+
+	server := &Server {closed: false}
+	go runServer(server, listener)
+
+	return server, nil
+}
+
+func (s *Server) Close() error {
+	s.closed = true
+	return nil
+}
